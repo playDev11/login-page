ab466e2fb93d2d38626f22c5545da5a44b2914ce
playDev11 <play.dev6@gmail.com>
2024-05-19 15:00:40 +0100

    feat: update on design

--- app.js
+++ app.js
@@ -8,4 +8,13 @@ registerBtn.addEventListener("click", ()=>{
 
 loginBtn.addEventListener("click", () => {
     container.classList.remove("active");
-})
+});
+
+function revealPassword(){
+    var passwordInput = document.getElementById("password")
+    var eyeIcon = document.querySelector(".reveal-password")
+    passwordInput.type == passwordInput.type === "password" ?
+    "text" : "password";
+    eyeIcon.classList.toggle("fa-eye-slash")
+    eyeIcon.classList.toggle("fa-eye")
+}
